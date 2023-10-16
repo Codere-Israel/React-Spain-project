2f539fbad6a0ca81eedb62a9ffe5a6cee8c5c497
Codere2022 <108279024+Codere2022@users.noreply.github.com>
2023-10-16 16:25:23 +0300

    commit

--- src/Components/seo/sports/cuotas/Futbol.jsx
+++ src/Components/seo/sports/cuotas/Futbol.jsx
@@ -264,6 +264,7 @@ export default function Futbol(props) {
                 ))}
               </div>
             ))}
+            <p>Prueba nuestro <a href="https://www.codere.es/ayuda/simulador-de-apuestas">simulador de apuestas</a></p>
           </div>
 
           <Fade
