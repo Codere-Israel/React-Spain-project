1ac1f6c37af4c7f1d2f1767c3b243c66ec47617f
MichaelDoro <Michael.doroshenko@codere.com>
2024-01-02 14:34:07 +0200

    s

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,6 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 module.exports = override(
 	addWebpackPlugin(new BundleAnalyzerPlugin({
-		analyzerPort: 9100 // or any other available port
+		analyzerPort: 9300 // or any other available port
 	}))
 );
