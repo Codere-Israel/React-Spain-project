1d78a5c38c2c6a6673e965d2d1c87c1369e6977f
MichaelDoro <Michael.doroshenko@codere.com>
2024-01-22 12:50:25 +0200

    confing

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,19 @@
 const { override, addWebpackPlugin } = require('customize-cra');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const portfinder = require('portfinder');
 
 module.exports = override(
 	(config) => {
-		portfinder.getPort({ port: 9400 }, (err, port) => {
-			if (err) {
-				throw new Error('No free ports found');
+		portfinder.getPort ({ port: 9400 }, (err, port) => {
+			if ( err ) {
+				throw new Error ('No free ports found');
 			}
 
 	addWebpackPlugin(new BundleAnalyzerPlugin({
-		analyzerPort: 9300 // or any other available port
-	}))
+				analyzerPort: port
+			}))(config);
+		});
+
+		return config;
+	}
 );
